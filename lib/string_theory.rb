ebaab25f0aac40a4cb7648fdc6e5fdd64261a1b3
deniznida <denizunat@hotmail.com>
2015-04-22 22:14:36 -0400

    passing the tests

--- lib/string_theory.rb
+++ lib/string_theory.rb
@@ -1,3 +1,38 @@
+require 'pry'
+
 def sort_by_appearance(characters_to_sort, long_text)
   # code goes here
+
+  character = characters_to_sort.split(%r{\s*})
+  text = long_text.split(%r{\s*})
+
+  hash = {}
+
+  character.each do |char_char|
+  	found = false
+  	text.each do |char_text|
+  		if char_char == char_text 
+  			found = true
+  			if hash.keys.include?(char_char)
+  				hash[char_char] +=1
+  			else
+  				hash[char_char] = 1
+  			end
+  		end
+  		if found == false
+  			hash[char_char] = 0
+  		end
+  	end
+  end
+  
+
+  sorted_hash = hash.sort_by {|k,v| v}.reverse
+
+  results_array = []
+
+  sorted_hash.collect do |k, v|
+  	results_array << k
+  end
+
+  results_array.join
 end
